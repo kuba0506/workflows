e8cce90f3bd57251c8728a68c9a8eaf07d460c8b
Kuba <kuba0506@gmail.com>
2015-07-20 22:42:05 +0200

    Łączenie i minifikacja JS

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,24 @@
  */
 var gulp = require('gulp'),
 	gutil = require('gulp-util'), //dodatkowe narzędzia jak log
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat  = require('gulp-concat'),
+	minify = require('gulp-uglify');
 
 /**
  * Sources
  */
-var projectResources = ['components/coffee/*.coffee',
-'components/scripts'];
+var coffeeSources = [
+	'components/coffee/*.coffee',
+	'components/scripts'
+	],
+
+	jsSources = [
+	'components/scripts/rclick.js',
+	'components/scripts/pixgrid.js',
+	'components/scripts/tagline.js',
+	'components/scripts/template.js'
+	];
 
 //Proste logowanie
 gulp.task('log', function () {
@@ -18,8 +29,16 @@ gulp.task('log', function () {
 
 //CoffeScript do JS
 gulp.task('coffee', function () {
-	gulp.src(projectResources[0])
+	gulp.src(coffeeSources[0])
 		.pipe(coffee({ bare: true })
 			.on('error', gutil.log)) //zapobiega wysypaniu się reszty zadań w przypadku błedu w kodzie
-			.pipe(gulp.dest(projectResources[1])) 
+			.pipe(gulp.dest(coffeeSources[1])) 
+});
+
+//Łączenie JS
+gulp.task('js', function () {
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(minify())
+	.pipe(gulp.dest('builds/development/js'))
 });
